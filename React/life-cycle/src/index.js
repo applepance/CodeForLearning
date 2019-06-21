01f9481b80f299b98814b028f5d09a4c315889f2
@applepance <421667507@qq.com>
2019-06-21 10:09:19 +0800

    React-life-cycle

---
+++ React/life-cycle/src/index.js
@@ -0,0 +1,14 @@
+// 构建用户界面 组件
+import React from 'react';
+// 把 react 组件 渲染到页面
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
