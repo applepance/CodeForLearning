79ffe5f8f1ab572a906b52341efa0bb32c20a66a
@applepance <421667507@qq.com>
2019-06-22 21:24:43 +0800

    redux

--- React/react-dynamicfield-redux-count/src/index.js
+++ React/react-dynamicfield-redux-count/src/index.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux'
 import './index.css';
 import App from './App';
+import Counter from './Counter'
+import countReducer from './reducer'
 import * as serviceWorker from './serviceWorker';
 
+function renderCount() {
+  ReactDOM.render(<Counter
+    value={store.getState()}
+    onIncrement={() => {
+      store.dispatch({ type: 'INCREMENT' })
+    }}
+    onDecrement={() => {
+      store.dispatch({ type: 'DECREMENT' })
+    }}
+    />, document.getElementById('redux'));
+}
+
+let store = createStore(countReducer);
 ReactDOM.render(<App />, document.getElementById('root'));
+renderCount();
+store.subscribe(() => {
+  renderCount();
+  console.log(store.getState());
+})
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
