27de666cc74c82b9eaf82192f3e298c3dee5800f
@applepance <421667507@qq.com>
2019-06-28 12:23:19 +0800

    typescript-router

---
+++ React/react-typescript-router/src/api/member/mockData.ts
@@ -0,0 +1,14 @@
+import { MemberEntity } from '../../model';
+
+export const members:MemberEntity[] = [
+  {
+    id: 1357912,
+    login: "brauliodiez",
+    avatar_url: "https://avatars.githubusercontent.com/u/1457912?v=3"
+  },
+  {
+    id: 1357902,
+    login: "brauliodiez",
+    avatar_url: "https://avatars.githubusercontent.com/u/4374977?v=3"
+  }
+]
