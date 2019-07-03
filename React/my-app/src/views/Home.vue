a19010a0be809f1d91589719c264abbe26b7cfa9
@applepance <421667507@qq.com>
2019-07-03 17:05:52 +0800

    my-app ts & vue

--- React/my-app/src/views/Home.vue
+++ React/my-app/src/views/Home.vue
@@ -6,15 +6,19 @@
 
 <script lang="ts">
 import Vue from 'vue';
+import { Component } from 'vue-property-decorator';
 import { Action } from 'vuex-class';
 
+@Component({})
 export default class Home extends Vue {
   private cate: string = 'in_theaters';
   private active: number = 0;
   private loading: boolean = true;
   private movieListData = null;
   @Action private movieList!: (cate: string) => any;
-
+  public constructor() {
+    super();
+  }
   private created() {
     this.movieList(this.cate)
       .then((res: any) => {
