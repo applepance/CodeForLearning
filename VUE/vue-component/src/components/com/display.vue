73edd4417d6b47deb543ecb2889e8ad22e83e9cd
@applepance <421667507@qq.com>
2019-07-10 11:52:07 +0800

    vue-component

---
+++ VUE/vue-component/src/components/com/display.vue
@@ -0,0 +1,18 @@
+<template>
+  <div ref="display">7788</div>
+</template>
+
+<script>
+export default {
+  props: {
+    code: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
