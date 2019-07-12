08b5096b0936d0d980a617d326c62e3611c66b89
@applepance <421667507@qq.com>
2019-07-12 16:41:55 +0800

    react-sourcecode-redux

---
+++ React/redux-source-example/src/redux/combineReducers.js
@@ -0,0 +1,23 @@
+function combineReducers (reducers){
+  const reducersKeys = Object.keys(reducers);
+  let finalReducers = {};
+  for(let i = 0, len = reducersKeys.length; i < len ; i++) {
+    let key = reducersKeys[i];
+    if(typeof reducers[key] === 'function') {
+      finalReducers[key] = reducers[key];
+    }
+  }
+  const finalReducersKeys = Object.keys(finalReducers);
+  const nextState = {}
+  return function (state = {}, action) {
+    for(let j = 0,len = finalReducersKeys.length; j < len; j++) {
+      const key = finalReducersKeys[j];
+      const reducer = finalReducersKeys[key];
+      const previousStateForKey = state[key];
+      const nextStateForKey = reducer(previousStateForKey, action);
+      nextState[key] = nextStateForKey;
+    }
+    return nextState;
+  }
+}
+export default combineReducers;
---
+++ React/redux-source-example/src/redux/index.js
@@ -0,0 +1,11 @@
+import createStore from './createStore';
+import combineReducers from './combineReducers';
+
+export default {
+  createStore,
+  combineReducers
+  // bindActionCreators,
+  // applyMiddleware,
+  // compose,
+  // __DO_NOT_USE__ActionTypes
+}
