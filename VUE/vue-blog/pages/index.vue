edf10920d489669c4909ee5bd0a21efe90634e90
@applepance <421667507@qq.com>
2019-07-19 22:42:58 +0800

    nuxt-vue-blog

---
+++ VUE/vue-blog/pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Welcome!</h1>
+    <NLink to="/about">
+    About Page
+    </NLink>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "Home Page"
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
