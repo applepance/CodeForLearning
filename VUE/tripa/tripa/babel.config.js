0962db6aed3e1723f5f7440e3928bcd8c8c152d6
@applepance <421667507@qq.com>
2019-07-25 12:17:44 +0800

    tripa

---
+++ VUE/tripa/tripa/babel.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  plugins: [
+    [
+      'import',
+      {
+        libraryName: 'mand-mobile',
+        libraryDirectory: 'lib'
+      }
+    ]
+  ]
+}
---
+++ VUE/tripa/tripa/src/components/RouterError/404.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="md-example-child md-example-child-result-page md-example-child-result-page-0">
+    <md-result-page
+      type="lost">
+    </md-result-page>
+  </div>
+</template>
+
+<script>
+import {ResultPage} from 'mand-mobile'
+
+export default {
+  name: 'result-page-demo',
+  components: {
+    [ResultPage.name]: ResultPage,
+  },
+}
+
+</script>
+
+<style lang="stylus">
+.md-example-child-result-page-0
+  background #FFF
+  position absolute
+  top 50%
+  left 50%
+  transform translate(-50%, -50%)
+  margin-top -100px
+</style>
